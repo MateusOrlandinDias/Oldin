dc3ab67c107118daac7ae859f70b63640c48eab2
MateusOrlandinDias <mateus.orlandin@gmail.com>
2023-08-26 20:52:00 -0300

    Home Page Finished

--- oldin-react/src/components/Secondary-Components/Product-Type-Card/index.js
+++ oldin-react/src/components/Secondary-Components/Product-Type-Card/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './styles.css';
 
-export default function ProductTypeCard() {
+export default function ProductTypeCard({ srcImgProductType, cardTitle }) {
     return (
         <div class="card-product-type">
             <div class="card-product-type-details">
-                <p class="product-type-text-title">Card title</p>
+                <p class="product-type-text-title">{cardTitle}</p>
+                <div className='images-types'>
+                    <img src={srcImgProductType} alt='product-type-photo' />
+                </div>
             </div>
             <button class="product-type-card-button">Ver nos produtos</button>
         </div>
--- oldin-react/src/components/Sections/Product-Type-Section/index.js
+++ oldin-react/src/components/Sections/Product-Type-Section/index.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import './styles.css';
 import ProductTypeCard from '../../Secondary-Components/Product-Type-Card';
+import imgGrampos from './../../../assets/Product-Types/grampos.png';
+import imgFitas from './../../../assets/Product-Types/fitas.png';
+import imgGrampeadores from './../../../assets/Product-Types/grampeador.png';
+import imgPinos from './../../../assets/Product-Types/pinos.png';
+import imgOutros from './../../../assets/Product-Types/outros.png';
 
 export default function ProductTypesSection() {
     return (
         <section className="product-types-section">
             <div className="product-types-row">
-                <ProductTypeCard />
-                <ProductTypeCard />
+                <ProductTypeCard
+                    srcImgProductType={imgGrampos}
+                    cardTitle='Grampos' />
+                <ProductTypeCard
+                    srcImgProductType={imgFitas}
+                    cardTitle='Fitas'
+                />
             </div>
             <div className="product-types-row">
-                <ProductTypeCard />
-                <ProductTypeCard />
+                <ProductTypeCard 
+                    srcImgProductType={imgGrampeadores}
+                    cardTitle='Grampeadores'
+                />
+                <ProductTypeCard 
+                    srcImgProductType={imgPinos}
+                    cardTitle='Pinos'
+                />
             </div>
             <div className="product-type-card other-product">
-                <ProductTypeCard />
+                <ProductTypeCard 
+                    srcImgProductType={imgOutros}
+                    cardTitle='Outros'
+                />
             </div>
         </section>
     );

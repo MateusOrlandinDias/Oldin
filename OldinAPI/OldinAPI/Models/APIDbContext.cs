6700e8aab859679560b69ec3cdaaf0319caa5eda
MateusOrlandinDias <mateus.orlandin@gmail.com>
2023-10-24 23:01:50 -0300

    Incompleted\ Stopen in connection string db

--- OldinAPI/OldinAPI/Models/APIDbContext.cs
+++ OldinAPI/OldinAPI/Models/APIDbContext.cs
@@ -1,7 +1,14 @@
-﻿namespace OldinAPI.Models
-{
-    public class APIDbContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace OldinAPI.Models
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions option) : base(option)
+        {
+            
+        }
+
+        public DbSet<Products> Products { get; set; }
     }
 }
--- OldinAPI/OldinAPI/Program.cs
+++ OldinAPI/OldinAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using OldinAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Dependency Injection of DbContext Class
+builder.Services.AddDbContext<APIDbContext>(options =>
+options.UseSqlServer());
 
 var app = builder.Build();
 

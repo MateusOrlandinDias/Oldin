d68eb706a4c60d9ea9bb70b1eb019ecb28f603d0
MateusOrlandinDias <mateus.orlandin@gmail.com>
2023-10-28 16:01:30 -0300

    Email Contact Section Init

---
+++ oldin-react/src/components/Sections/Email-Contact-Section/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import animationData from "../../../assets/Animations/animation_loadxisd.json";
+
+export default function EmailContactSection() {
+  return (
+    <div className="email-contact-section">
+
+    </div>
+  );
+}
--- oldin-react/src/pages/Contact/index.js
+++ oldin-react/src/pages/Contact/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import './styles.css';
 import Header from '../../components/Sections/Header';
 import Lettering from '../../components/Secondary-Components/Lettering';
+import EmailContactSection from "../../components/Sections/Email-Contact-Section";
 
 export default function Contact() {
     return (
         <div>
             <Header />
             <Lettering />
+            <EmailContactSection />
         </div>
     );
 }

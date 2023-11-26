b7aee89e7226ab5d9cfb18bf89f47f64cec3dc53
MateusOrlandinDias <mateus.orlandin@gmail.com>
2023-11-26 14:46:59 -0300

    work with us pt2

---
+++ oldin-react/src/components/Sections/Work-With-Us-Section/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+export default function WorkWithUsSection(){
+    return(
+        <section className='work-with-us-section'>
+            
+        </section>
+    );
+}
--- oldin-react/src/pages/Work-with-us/index.js
+++ oldin-react/src/pages/Work-with-us/index.js
@@ -3,12 +3,14 @@ import './styles.css';
 import Header from '../../components/Sections/Header';
 import Lettering from '../../components/Secondary-Components/Lettering';
 import Footer from "../../components/Sections/Footer";
+import WorkWithUsSection from "../../components/Sections/Work-With-Us-Section";
 
 export default function WorkWithUs() {
     return (
         <div>
             <Header />
             <Lettering />
+            <WorkWithUsSection />
             <Footer />
         </div>
     );

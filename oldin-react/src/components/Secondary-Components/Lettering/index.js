d779d84ca0007596283ce374886e8a3c6d8062b2
MateusOrlandinDias <mateus.orlandin@gmail.com>
2023-12-02 17:48:50 -0300

    Aviso by API

--- oldin-react/src/components/Secondary-Components/Lettering/index.js
+++ oldin-react/src/components/Secondary-Components/Lettering/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../../services/api';
 import './styles.css';
 
-const text = 'AVISO: O site está em manutenção.'
+export default function Lettering() {
+    const [letteringText, setLetteringText] = useState([]);
+    const [text, setText] = useState('');
 
-export default function Lettering(){
-    return(
+    useEffect(() => {
+        // Load cache at initialization of component
+        const cachedData = localStorage.getItem('letteringData');
+        if (cachedData) {
+            setLetteringText(JSON.parse(cachedData));
+        }
+
+        // Update lettering every 2 minutes
+        const interval = setInterval(() => {
+            api.get('/avisos_ativos')
+                .then(response => {
+                    setLetteringText(response.data);
+                    // Store data in cache
+                    localStorage.setItem('letteringData', JSON.stringify(response.data));
+                })
+                .catch(error => {
+                    console.error('Erro ao obter dados do lettering:', error);
+                });
+        }, 120000); // 120000 milisseconds = 2 minutes
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        // Update text only if has data in cache
+        if (letteringText.length > 0) {
+            setText(letteringText[0].aviso);
+        }
+    }, [letteringText]);
+
+    return (
         <div className='lettering'>
             <span>{text}</span>
         </div>
